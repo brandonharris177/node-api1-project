a707d91fab9f3ee021d198e473f508a0eb3026af
Brandon-Harris <brandonharris177@gmail.com>
2019-09-28 18:36:45 -0600

    postman working

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // implement your API here
 const express = require('express'); // import the express package
+const data = require('./data/db')
 
 const server = express(); // creates the server
 
 // handle requests to the root of the api, the / route
 server.get('/', (req, res) => {
-  res.send('Hello from Express');
+  res.send('Its working');
 });
 
 // watch for connections on port 5000
 server.listen(5000, () =>
   console.log('Server running on http://localhost:5000')
 );
+
+server.get('/api/users', (req, res) => {
+  data.find();
+    res.end();
+  });
