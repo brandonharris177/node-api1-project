cd3fbaad2f90bdb7db5bf061b5dc2a813674c5c6
Brandon-Harris <brandonharris177@gmail.com>
2019-09-30 14:49:36 -0600

    MVP

--- index.js
+++ index.js
@@ -56,17 +56,44 @@ server.get(`/api/users/:id`, (req, res) => {
     })
   });  
 
-  server.delete(`/api/users/:id`, (req, res) => {
-    data.findById(req.params.id)
-      .then(user => {
-        console.log(user)
-        if (user) {
-          data.remove(user)
-          res.status(200).json({ message: `${user.name} Deleted.` })
-        } else {
-          res.status(404).json({ message: "The user with the specified ID does not exist." })
-        }
-      }).catch(error => {
-        res.status(500).json({ error: "The user information could not be retrieved." })
-      })
-    });  
+server.delete(`/api/users/:id`, (req, res) => {
+  data.findById(req.params.id)
+    .then(user => {
+      console.log(user)
+      if (user) {
+        data.remove(user)
+        res.status(200).json({ message: `${user.name} Deleted.` })
+      } else {
+        res.status(404).json({ message: "The user with the specified ID does not exist." })
+      }
+    }).catch(error => {
+      res.status(500).json({ error: "The user could not be removed" })
+    })
+  });  
+    
+server.put('/api/users/:id', (req, res) => {
+  const userData =  req.body
+  const id = req.params.id
+  // console.log(req.body)
+  // console.log(`user Data`, userData
+  if (req.body.name && req.body.bio) {
+    // console.log(`user Data`, userData)
+    data.update(id, userData)
+    .then(response => {
+      // console.log(`id`, id)
+      if (response === 1) {
+        // console.log(id)
+        data.findById(id)
+        .then(updatedUser => {
+        //  console.log(`updated User`, updatedUser)
+        res.status(200).json(updatedUser)})
+      } else {
+        res.status(500).json({ error: "Server Error" })
+      }}).catch(error => {
+      res.end()
+      res.status(500).json({ error: "The user information could not be modified." })
+    })
+  } else {
+    res.end()
+    res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+  }});
